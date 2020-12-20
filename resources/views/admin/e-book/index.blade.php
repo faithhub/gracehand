2e6d92ef4c78c7a779dc73c4326b921bc1772842
faithdinno <adebayooluwadara@gmail.com>
2020-12-20 12:59:14 +0100

    1st

--- resources/views/admin/e-book/index.blade.php
+++ resources/views/admin/e-book/index.blade.php
@@ -5,12 +5,12 @@
     <div class="col-lg-12">
         <div class="breadcrumb-content d-flex flex-wrap justify-content-between align-items-center">
             <div class="section-heading">
-                <h2 class="sec__title line-height-45">Users</h2>
+                <h2 class="sec__title line-height-45">Courses</h2>
             </div><!-- end section-heading -->
             <ul class="list-items d-flex align-items-center">
                 <li class="active__list-item"><a href="index.html">Home</a></li>
                 <li class="active__list-item">Dashboard</li>
-                <li>Users</li>
+                <li>Courses</li>
             </ul>
         </div><!-- end breadcrumb-content -->
     </div><!-- end col-lg-12 -->
@@ -20,27 +20,31 @@
     <div class="col-lg-12">
         <div class="billing-form-item">
             <div class="billing-title-wrap">
-                <h3 class="widget-title pb-0">All Users</h3>
+                <h3 class="widget-title pb-0">All Courses</h3>
                 <div class="title-shape margin-top-10px"></div>
+                <div class="text-right">                    
+                    <a href="{{ url('admin/add-course') }}" class="btn btn-success">Add New Course</a>
+                </div>
             </div><!-- billing-title-wrap -->
             <div class="billing-content pb-0">
-                    @if ($users->count() > 0)
+                    @if ($courses->count() > 0)
                         <div class="table-responsive">
                             <table class="table" id="myTable" width="100%">
                                 <thead>
                                     <tr>
                                         <th>S/N</th>
-                                        <th width="40%">User</th>
+                                        <th>User</th>
+                                        <th>Status</th>
                                         <th>Referred By</th>
                                         <th>Date Registered</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                @foreach ($users as $user)                            
+                                @foreach ($courses as $user)                            
                                     <tr>
                                         <td>{{$sn++}}</td>
-                                        <td width="40%">                                        
+                                        <td>                                        
                                             <div class="bread-details d-flex">
                                                 <div class="bread-img flex-shrink-0">
                                                     <a href="#" class="d-block">
@@ -56,21 +60,17 @@
                                                     <p class="mt-1 font-size-15">
                                                         <span class="mr-2"><i class="la la-map-marker mr-1"></i>{{$user->address ?? 'Not Uploaded Yet'}}</span>
                                                     </p>
-                                                    
-                                                    <p class="mt-1 font-size-15">
-                                                        <span class="mr-2"><i class="la la-user mr-1"></i>
-                                                        
-                                                    @if ($user->status == 'Active')
-                                                    <span class="badge badge-success p-1">{{$user->status}}</span>                                            
-                                                    @elseif ($user->status == 'Blocked')
-                                                    <span class="badge badge-danger p-1">{{$user->status}}</span>
-                                                    @else
-                                                    <span class="badge badge-warning p-1">{{$user->status}}</span>                                            
-                                                    @endif
-                                                        </span>
-                                                    </p>
                                                 </div><!-- end manage-candidate-content -->
                                             </div>
+                                        </td>
+                                        <td>
+                                            @if ($user->status == 'Active')
+                                                <span class="badge badge-success p-1">{{$user->status}}</span>                                            
+                                            @elseif ($user->status == 'Blocked')
+                                            <span class="badge badge-danger p-1">{{$user->status}}</span>
+                                            @else
+                                            <span class="badge badge-warning p-1">{{$user->status}}</span>                                            
+                                            @endif
                                         </td>
                                         <td></td>
                                         <td>{{ date('D, M j, Y \a\t g:ia', strtotime($user->created_at))}}</td>
@@ -108,7 +108,7 @@
                         </div>
                     @else
                         <div class="text-center mb-4 mt-3">
-                            <h2 class="text-black" style="color: black; font-family: Georgia, 'Times New Roman', Times, serif">No User Registered yet, Check back later</h2>
+                            <h2 class="text-black" style="color: black; font-family: Georgia, 'Times New Roman', Times, serif">No Courses Created yet, Check back later</h2>
                         </div>
                     @endif
                 </div>
